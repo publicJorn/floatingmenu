5681b24c90affbe8226b26764052dc6c525df21c
Jorn Luiten <js.luiten@gmail.com>
2016-08-25 11:08:37 +0200

    Float baby

--- src/FloatingMenu/FloatingMenu.js
+++ src/FloatingMenu/FloatingMenu.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import MenuIcon from './MenuIcon';
 import MenuItemList from './MenuItemList';
+import classnames from 'classnames';
 
 require('./FloatingMenu.less');
 
 export class FloatingMenu extends Component {
   constructor (props) {
     super(props);
-    this.state = {open: this.props.open || false};
+
+    // Position on initial load of page
+    this.relativeDomScrollPosition = NaN;
+    this.state = {
+      open: this.props.open || false,
+      floating: false
+    };
+  }
+
+  componentDidMount () {
+    this.relativeDomScrollPosition = this._menu.getBoundingClientRect().top;
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll (evt) {
+    let scrolledFromTop = event.srcElement.body.scrollTop;
+    let menuPosition = this._menu.getBoundingClientRect();
+
+    if (scrolledFromTop > this.relativeDomScrollPosition && !this.state.floating) {
+      this.setState({ floating: true });
+    } else
+    if (scrolledFromTop <= this.relativeDomScrollPosition && this.state.floating) {
+      this.setState({ floating: false });
+    }
   }
 
   toggleOpenState () {
@@ -19,17 +47,21 @@ export class FloatingMenu extends Component {
   }
 
   render () {
+    const classes = classnames('FloatingMenu', {'FloatingMenu--floating': this.state.floating});
+
     return (
-      <div className="FloatingMenu">
-        <MenuIcon
-          open={this.state.open}
-          onRequestToggleOpenState={this.toggleOpenState.bind(this)}
-        />
-        <MenuItemList
-          items={this.props.menuItems}
-          open={this.state.open}
-          onItemClicked={this.toggleOpenState.bind(this)} // TODO
-        />
+      <div className={classes} ref={(c) => this._menu = c}>
+        <div className="FloatingMenu-Inner">
+          <MenuIcon
+            open={this.state.open}
+            onRequestToggleOpenState={this.toggleOpenState.bind(this)}
+          />
+          <MenuItemList
+            items={this.props.menuItems}
+            open={this.state.open}
+            onItemClicked={this.toggleOpenState.bind(this)} // TODO
+          />
+        </div>
       </div>
     );
   }
@@ -39,11 +71,11 @@ export class FloatingMenu extends Component {
 /**
  * Searches current DOM for menu items.
  * Should be links with this format: `<a href="#ItemName"></a>`
- * @return {Array} List of `name` attibutes
+ * @return {Array} List of names
  */
 export function findMenuItemsInDom (selector = '[data-menuItem]') {
   const menuItemLinks = document.querySelectorAll(selector);
-  const menuItems = Array.prototype.map.call(menuItemLinks, (el) => {
+  const menuItems = Array.from(menuItemLinks, (el) => {
     return el.getAttribute('name');
   });
 
